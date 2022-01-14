198cae731b80c2e6331cef522d4fb457f1772fe1
Nico <nico@Nicos-MBP.fios-router.home>
2022-01-14 02:03:08 -0500

    add win condition, shuffle array

--- puzzle/index.js
+++ puzzle/index.js
@@ -4,7 +4,7 @@
     0: [1, 3],
     1: [0, 2, 4],
     2: [1, 5],
-    3: [0, 4],
+    3: [0, 4, 6],
     4: [1, 3, 5, 7],
     5: [2, 4, 8],
     6: [3, 7],
@@ -17,6 +17,23 @@
     [3, 4, 5],
     [6, 7, null]
   ];
+
+  const init = () => {
+    const button = document.createElement('button');
+    button.innerHTML = 'Shuffle';
+    button.addEventListener('click', () => {
+      shuffleArray();
+      render();
+    });
+    document.getElementsByTagName('figure')[0].appendChild(button);
+
+    for (let i = 0; i < tiles.length; i++) {
+      const tile = tiles[i];
+
+      setTimeout(() => tile.classList.add('animate'), 100);
+      tile.addEventListener('click', handleClick(i));
+    }
+  }
 
   const getPosition = index => {
     for (let i = 0; i < state.length; i++) {
@@ -51,6 +68,22 @@
     tempState[toRow][toCol] = prevTile;
 
     state = tempState;
+
+    checkForWin();
+  }
+
+  const checkForWin = () => {
+    if (state[0][0] === 0
+      && state[0][1] === 1
+      && state[0][2] === 2
+      && state[1][0] === 3
+      && state[1][1] === 4
+      && state[1][2] === 5
+      && state[2][0] === 6
+      && state[2][1] === 7
+    ) {
+      document.getElementsByTagName('figcaption')[0].innerHTML = 'A Puzzle - You Win!';
+    }
   }
 
   const handleClick = number => () => {
@@ -67,15 +100,27 @@
     }
   }
 
-  const render = (init = false) => {
+  const shuffleArray = () => {
+    let shuffled = [0, 1, 2, 3, 4, 5, 6, 7, null]
+      .map((value) => ({ value, sort: Math.random() }))
+      .sort((a, b) => a.sort - b.sort)
+      .map(({ value }) => value);
+
+    state = [
+      shuffled.slice(0, 3),
+      shuffled.slice(3, 6),
+      shuffled.slice(6, 9),
+    ];
+  }
+
+  const render = () => {
     for (let i = 0; i < tiles.length; i++) {
       const tile = tiles[i];
-
-      if (init) tile.addEventListener('click', handleClick(i));
 
       tile.style.cssText = getStyleForPosition(getPosition(i));
     }
   }
 
-  render(true);
+  render();
+  init();
 })();
